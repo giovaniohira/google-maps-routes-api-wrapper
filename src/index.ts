86838fe11ff798b7196266bc569869a67bd4111b
Giovani Ohira <giovanirossi863@gmail.com>
2025-09-23 00:29:29 -0300

    feat: add input validation and typed errors

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Core client
+export { RoutesClient } from './core/routesClient';
+
+// Types
+export * from './types';
+
+// HTTP Adapters
+export { FetchAdapter } from './adapters/http/fetchAdapter';
+export { HttpAdapter, HttpRequest, HttpResponse } from './adapters/http/httpAdapter';
+
+// Errors
+export { RoutesError } from './errors';
+
+// Validation
+export * from './validation';
